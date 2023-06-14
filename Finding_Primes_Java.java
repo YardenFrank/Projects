af07609148ed0ab6f36d4ebf64cced53385b1b5a
YardenFrank <136602637+YardenFrank@users.noreply.github.com>
2023-06-14 17:19:25 +0300

    Add files via upload

---
+++ Finding_Primes_Java.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class Main {
+    public static Scanner reader = new Scanner(System.in);
+    public static void main(String[] args){
+        int num = (-1);
+        while(num<1) {
+            System.out.println("Enter a number:");
+            num = reader.nextInt();
+        }
+        System.out.println("factors: "+numInPrime(num));
+    }
+    public static boolean isPrime(int num)
+    {
+        for(int i = 2; i<=(Math.sqrt(num)); i++)
+        {
+            if(num%i==0)
+                return false;
+        }
+        return(num!=1);
+    }
+    public static int nextPrime(int num)
+    {
+        num++;
+        if(num%2==0)
+            num++;
+        while(!isPrime(num))
+            num+=2;
+        return num;
+    }
+    public static String numInPrime(int num)
+    {
+        if(num==1)
+            return "1 has no prime factors.";
+        String numinprime = "";
+        int checker = 2, limitnum = (int)Math.sqrt(num), counter = 0;
+        for(; checker<=limitnum; checker = nextPrime(checker))
+        {
+            while(num%checker==0) {
+                counter++;
+                num /= checker;
+            }
+            if(counter>1)
+                numinprime+=checker+"^"+counter+" ";
+            else {
+                if (counter > 0)
+                    numinprime+=checker+" ";
+            }
+            counter = 0;
+        }
+        if(num!=1)
+            numinprime+=num;
+        return numinprime;
+    }
+}
+
